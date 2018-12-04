671a73ca7b791fc2dd628ba1910ff944d55ca639
crisyeah <cristian.cienfuegos@outlook.com>
2018-12-04 14:28:08 -0600

    Add last humidity action.

--- Invernify/ClientApp/src/services/humidity-service.ts
+++ Invernify/ClientApp/src/services/humidity-service.ts
@@ -5,4 +5,8 @@ export default class HumidityService {
     public async sampleHumidity(): Promise<Humidity[]> {
         return axios.get('api/iot/humidity').then(x => x.data);
     }
+
+    public async lastHumidity(): Promise<Humidity | null> {
+        return axios.get('api/iot/last-humidity').then(x => x.data);
+    }
 }
