81a22d50fa16a0bbd6ea29211dd92c43a6b77a08
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-12-04 12:26:03 -0600

    Se ha agregado modal y maquetación

--- markup/infoVeggie.js
+++ markup/infoVeggie.js
@@ -1,17 +1,13 @@
-swal({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-    if (result.value) {
-      swal(
-        'Deleted!',
-        'Your file has been deleted.',
-        'success'
-      )
-    }
-  })
+let verdura = document.getElementById("verdura");
+
+verdura.addEventListener("click", function(event){
+    event.preventDefault();
+    swal({
+        title: 'Calabaza',
+        text: 'Sabías que el peso comercial varia de 1.2 a 1.5 kg, pero puede llegar a pesar hasta 3.5 kg. El ciclo del cultivo varía de 90 a 120 días. El corte del fruto se lleva a cabo cuando está completamente maduro .',
+        icon: 'https://github.com/JudithCortes/invernify/issues/1',
+        imageWidth: 400,
+        imageHeight: 200,
+      
+      });
+});
