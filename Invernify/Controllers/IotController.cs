f5ae8401afe424c48eb45fc90173cb8b3e724357
crisyeah <cristian.cienfuegos@outlook.com>
2018-12-04 02:11:48 -0600

    Simple IOT controller API controller.

---
+++ Invernify/Controllers/IotController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Invernify.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invernify.Controllers
+{
+    [Route("api/iot")]
+    public class IotController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public IotController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("sample")]
+        public string GetIotData()
+        {
+            return _context.DeviceData.Count().ToString();
+        }
+    }
+}
